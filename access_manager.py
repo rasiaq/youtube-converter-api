b54edbf9b51566fc1f156db253933d97c402506b
Piotr Rasiński <246764@student.pwr.edu.pl>
2021-03-31 10:55:16 +0200

    Added some docs, renamed some methods and variables

--- access_manager.py
+++ access_manager.py
@@ -8,28 +8,59 @@ audio_files = {}
 
 
 def add_token(token, file):
-    expiry_date = datetime.now() + timedelta(seconds=30)
+    """
+    Adds token with expiration date to allowed_tokens and token with file name to audio_files
+    :param token: generated access token
+    :param file: audio file name
+    :return:
+    """
+    expiry_date = datetime.now() + timedelta(minutes=constants.EXPIRY_TIME_MINUTES)
     allowed_tokens[token] = expiry_date
     audio_files[token] = file
 
 
 def has_access(token):
+    """
+    Checks if token exists in allowed_tokens
+    :param token: access token
+    :return: True if is present in allowed_tokens, False otherwise
+    """
     return token in allowed_tokens
 
 
 def is_valid(token):
+    """
+    Checks if token has not expired
+    :param token: access token
+    :return: True if token expired, False otherwise
+    """
     return allowed_tokens[token] >= datetime.now()
 
 
-def file_exists(token):
+def file_assigned(token):
+    """
+    Checks if file is associated with a token
+    :param token: access token
+    :return: True if file is assigned, False otherwise
+    """
     return token in audio_files
 
 
 def get_audio_file(token):
+    """
+    Returns audio file name
+    :param token: access token
+    :return: audio file name
+    """
     return audio_files[token]
 
 
 def remove_expired_tokens():
+    """
+    Adds expired token to expired_tokens array, then removes them.
+    Pops file names to array, which are suppose to be deleted
+    :return: array of file names to be deleted
+    """
     expired_tokens = []
     files_to_delete = []
     for token in allowed_tokens:
@@ -44,11 +75,20 @@ def remove_expired_tokens():
 
 
 def delete_expired_files(files):
+    """
+    Removes files which were previously associated with expired tokens
+    :param files: file names to be deleted
+    :return:
+    """
     for file in files:
         os.remove(constants.DOWNLOADS_DIRECTORY + file)
 
 
 def manage_tokens():
+    """
+    Checks if there are any expired tokens. If so, removes them and deletes a associated files
+    :return:
+    """
     while True:
         files = remove_expired_tokens()
         delete_expired_files(files)
--- main.py
+++ main.py
@@ -10,16 +10,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def download_audio():
+    # TODO add format changing to .mp3
+    """
+    Downloads audio track from given url and saves it to specified directory in .mp4 format
+    :return: previously generated access token
+    """
     url = request.args.get('url')
     stream = pytube.YouTube(url).streams
     audio_stream = stream.get_audio_only()
-    file = audio_stream.download(output_path=DOWNLOADS_DIRECTORY)
-    file_name = strip_file_name(file)
+    file_path = audio_stream.download(output_path=DOWNLOADS_DIRECTORY)
+    file_name = extract_file_name(file_path)
     return generate_access_token(file_name)
 
 
 @app.route('/download', methods=['GET', 'POST'])
 def get_audio():
+    """
+    After checking all conditions related to given token, returns audio file associated with it
+    :return: audio file associated with specified token.
+    If FileNotFoundError is caught, then json response with error message
+    """
     token = request.args.get('token')
     if not access_manager.has_access(token):
         return jsonify(message='Given token is invalid'), UNAUTHORIZED
@@ -35,13 +45,23 @@ def get_audio():
 
 
 def generate_access_token(file_name):
-    token = secrets.token_urlsafe(20)
+    """
+    Generates access token of given length, then sends it to access_manager.add_token with associated file
+    :param file_name: file name with which the token is to be associated
+    :return: access token
+    """
+    token = secrets.token_urlsafe(TOKEN_LENGTH)
     access_manager.add_token(token, file_name)
     return jsonify(token=token)
 
 
-def strip_file_name(file):
-    return file[len(ABS_DOWNLOADS_PATH):]
+def extract_file_name(file_path):
+    """
+    Extracts filename from given path
+    :param file_path: absolute path to the file containing file name
+    :return: filename extracted from file_path
+    """
+    return file_path[len(ABS_DOWNLOADS_PATH):]
 
 
 def main():
